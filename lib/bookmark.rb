76a0bf662d417ac672eb89d14295d546e9c8e7e0
Chenbo Yi <yichenbo@outlook.com>
2019-04-25 22:32:08 +0100

    extract the database setup

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,4 +1,4 @@
-require 'pg'
+require_relative 'database_connection'
 
 class Bookmark
   attr_reader :id, :title, :url
@@ -10,9 +10,7 @@ class Bookmark
   end
 
   def self.all
-    connection = valid_connection
-
-    result = connection.exec('SELECT * FROM bookmarks;')
+    result = DatabaseConnection.query('SELECT * FROM bookmarks;')
     result.map do |bookmark|
       Bookmark.new(id: bookmark['id'],
                    title: bookmark['title'],
@@ -21,9 +19,7 @@ class Bookmark
   end
 
   def self.create(url:, title:)
-    connection = valid_connection
-
-    result = connection.exec("INSERT INTO bookmarks (url, title)
+    result = DatabaseConnection.query("INSERT INTO bookmarks (url, title)
                               VALUES('#{url}', '#{title}')
                               RETURNING id, url, title;")
     Bookmark.new(id: result[0]['id'], title: result[0]['title'],
@@ -31,29 +27,20 @@ class Bookmark
   end
 
   def self.delete(id)
-    connection = valid_connection
-    connection.exec("DELETE FROM bookmarks WHERE id = #{id}")
+    DatabaseConnection.query("DELETE FROM bookmarks WHERE id = #{id}")
   end
 
   def self.update(id:, title:, url:)
-    connection = valid_connection
-    result = connection.exec("UPDATE bookmarks SET title = '#{title}',
+    result = DatabaseConnection.query("UPDATE bookmarks SET title = '#{title}',
                     url = '#{url}' WHERE id = #{id} RETURNING id, url, title;")
     Bookmark.new(id: result[0]['id'], title: result[0]['title'],
                  url: result[0]['url'])
   end
 
   def self.find(id:)
-    connection = valid_connection
-    result = connection.exec("SELECT * FROM bookmarks WHERE id = #{id};")
+    result = DatabaseConnection.query("SELECT * FROM bookmarks
+                                      WHERE id = #{id};")
     Bookmark.new(id: result[0]['id'], title: result[0]['title'],
                  url: result[0]['url'])
-  end
-
-  def self.valid_connection
-    condition = (ENV['ENVIRONMENT'] == 'test')
-    return PG.connect(dbname: 'bookmark_manager_test') if condition
-
-    PG.connect(dbname: 'bookmark_manager')
   end
 end
